21afa0bd129ab07a5cf5d142c197af0a768645a8
Ctomes <72369162+Ctomes@users.noreply.github.com>
2021-11-05 00:28:48 -0700

    Add files via upload

---
+++ GroupProjCS3560num2/Forms/StartPage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GroupProjCS3560num2.Forms
+{
+    public partial class StartPage : Form
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            PinPage f = new PinPage();
+            f.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Login f = new Login();
+            f.Show();
+        }
+    }
+}
