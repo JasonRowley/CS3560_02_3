de38d5d55b7dad5708cae273c2f893f9f44d68b2
Jason Rowley <jasonrowley98@gmail.com>
2021-11-12 17:12:40 -0800

    Pulled from Vu_branch

--- GroupProjCS3560num2/Classes/Employee.cs
+++ GroupProjCS3560num2/Classes/Employee.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Text;
+
 
 namespace GroupProjCS3560num2.Classes
 {
     class Employee
     {
+<<<<<<< HEAD
         int employeeID;
         int jobID;
         string pw;
@@ -45,12 +48,80 @@ namespace GroupProjCS3560num2.Classes
             this.sSN = sSN;
             this.adjustment = adjustment;
         }
+=======
 
-        public Employee(int employeeID)
+        private int employeeID;
+        private int jobID;
+        private string pw;
+        private string empName;
+        private string physicalAddress;
+        private string emailAddress;
+        private int phoneNumber;
+        private string dateOfBirth;
+        private string bankAccNumber;
+        private string sSN;
+        private double adjustment;
+
+
+
+        public Employee(int insertEmployeeID)
         {
+            getEmployeeInformation(insertEmployeeID);
+        }
+
+
+
+
+>>>>>>> c350faba0d781b5ec352aafb4f30ec71b1b0fdf2
+
+        private void getEmployeeInformation(int insertEmployeeID)
+        {
+<<<<<<< HEAD
             this.employeeID = employeeID;
         }
 
+=======
+     
+            string conn = "server=localhost; userid=root; password=  " + pw1 + "; database = employee_schema";    //<---- ask to make a class to replace this line for security
+            using (var con = new MySqlConnection(conn))
+            {
+                string str = string.Format("select * from Employee where employeeID = " + insertEmployeeID);
+                using (var cmd = new MySqlCommand(str, con))
+                {
+                    con.Open();
+
+                    MySqlDataReader myReader = cmd.ExecuteReader();
+                    try
+                    {
+                        // Always call Read before accessing data.
+                        while (myReader.Read())
+                        {
+                            this.jobID = myReader.GetInt32("jobID");
+                            this.pw = myReader.GetString("pw");
+                            this.empName = myReader.GetString("empName");
+                            this.physicalAddress = myReader.GetString("physicalAddress");
+                            this.emailAddress = myReader.GetString("emailAddress");
+                            this.phoneNumber = myReader.GetInt32("phoneNumber");
+                            //this.dateOfBirth = myReader.GetString("dateOfBirth");    <--- adjust later            
+                            this.bankAccNumber = myReader.GetString("bankAccNumber");
+                            this.sSN = myReader.GetString("sSN");
+                            this.adjustment = myReader.GetDouble("adjustment");
+                        }
+                    }
+                    finally
+                    {
+                        // always call Close when done reading.
+                        myReader.Close();
+
+                        // Close the connection when done with it.
+                        con.Close();
+                    }
+                }
+            }
+        }
+
+        //public getters
+>>>>>>> c350faba0d781b5ec352aafb4f30ec71b1b0fdf2
         public int getEmployeeID()
         {
             return employeeID;
@@ -85,7 +156,11 @@ namespace GroupProjCS3560num2.Classes
             return phoneNumber;
         }
 
+<<<<<<< HEAD
         public DateTime getDateOfBirth()
+=======
+        public string getDateOfBirth()
+>>>>>>> c350faba0d781b5ec352aafb4f30ec71b1b0fdf2
         {
             return dateOfBirth;
         }
@@ -105,20 +180,65 @@ namespace GroupProjCS3560num2.Classes
             return adjustment;
         }
 
+<<<<<<< HEAD
         public void setAdjustment(double adj)
         {
             this.adjustment = adj;
         }
     }
+=======
 
 
+>>>>>>> c350faba0d781b5ec352aafb4f30ec71b1b0fdf2
+
+        private void setFunctionHelper<T>(int employeeID, string tableAttribute, T insertChange)
+        {
+            //ConnectSql sqlConnection = new ConnectSql();
+            //string con = sqlConnection.open();
+
+            string con = "server=localhost; userid=root; password=  " + pw1 + "; database = employee_schema";
+            using var sqlCon = new MySqlConnection(con);
+            sqlCon.Open();
+            using var cmd = new MySqlCommand();
+            cmd.Connection = sqlCon;
+            cmd.CommandText = string.Format("update employee set " + tableAttribute + " = '{0}'  where employeeID = {1}", insertChange, employeeID);
+            cmd.ExecuteNonQuery();
+
+        }
 
 
+        //public setters
+        public void setEmployeeID(int employeeID, int newID)
+        {
+            setFunctionHelper(employeeID, "employeeID", newID);
+        }
 
+        public void setJobID(int employeeID, int newJobID)
+        {
+            setFunctionHelper(employeeID, "jobID", newJobID);
+        }
 
+        public void setPw(int employeeID, string newPW)
+        {
+            setFunctionHelper(employeeID, "pw", newPW);
+        }
 
+        public void setEmpName(int employeeID, string newName)
+        {
+            setFunctionHelper(employeeID, "empName", newName);
+        }
 
+        public void setPhysicalAddress(int employeeID, string newAddress)
+        {
+            setFunctionHelper(employeeID, "physicalAddress", newAddress);
+        }
 
+        public void setEmailAddress(int employeeID, string newEmail)
+        {
+            setFunctionHelper(employeeID, "emailAddress", newEmail);
+        }
+
+<<<<<<< HEAD
         // MySql connection function
         //private void connectSql(string insertCmdLine)
         //{
@@ -143,7 +263,35 @@ namespace GroupProjCS3560num2.Classes
                 
         //    }
         //}
+=======
+        public void setPhoneNumber(int employeeID, long newPhoneNumber)         // <======= phone may need to be set to long
+        {
+            setFunctionHelper(employeeID, "phoneNumber", newPhoneNumber);
+        }
 
+        public void setDateOfBirth(int employeeID, string newDateOFBirth)       // <==== adjust later 
+        {
+            setFunctionHelper(employeeID, "dateOfBirth", newDateOFBirth);
+        }
+
+        public void setBankAccNumber(int employeeID, string newBankNumber)
+        {
+            setFunctionHelper(employeeID, "bankAccNumber", newBankNumber);
+        }
+
+        public void setsSN(int employeeID, string newEmployeeID)
+        {
+            setFunctionHelper(employeeID, "sSN", newEmployeeID);
+        }
+
+        public void setAdjustment(int employeeID, double newAdjustment)
+        {
+            setFunctionHelper(employeeID, "adjustment", newAdjustment);
+        }
+>>>>>>> c350faba0d781b5ec352aafb4f30ec71b1b0fdf2
+
+
+    }
 
 
 
